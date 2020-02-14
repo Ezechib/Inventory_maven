6a54bc36e5283880cc59e37bf0e5a5b92d25cd8d
Ezechib <60218808+Ezechib@users.noreply.github.com>
2020-02-14 12:30:29 +0000

    Add files via upload

---
+++ src/main/java/com/qa/ims/persistence/dao/OrderDaoMysql.java
@@ -0,0 +1,112 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Order;
+
+public class OrderDaoMysql implements Dao<Order> {
+
+	public static final Logger LOGGER = Logger.getLogger(OrderDaoMysql.class);
+
+	private String jdbcConnectionUrl;
+	private String username;
+	private String password;
+
+	public OrderDaoMysql(String username, String password) {
+		this.jdbcConnectionUrl = "jdbc:mysql://35.239.193.215:3306/projectdb";
+		this.username = username;
+		this.password = password;
+	}
+
+	public OrderDaoMysql(String jdbcConnectionUrl, String username, String password) {
+		this.jdbcConnectionUrl = jdbcConnectionUrl;
+		this.username = username;
+		this.password = password;
+	}
+
+	Order orderFromResultSet(ResultSet resultSet) throws SQLException {
+		Long order_id = resultSet.getLong("order_id");
+		Long customer_id = resultSet.getLong("customer_id");
+		Long total_cost = resultSet.getLong("total_cost");
+		Long number_of_items = resultSet.getLong("number_of_items");
+		return new Order(order_id, customer_id, total_cost, number_of_items);
+	}
+
+	/**
+	 * Reads all customers from the database
+	 *
+	 * @return A list of customers
+	 */
+
+	@Override
+	public List<Order> readAll() {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("select * from Orders");) {
+			ArrayList<Order> orders = new ArrayList<>();
+			while (resultSet.next()) {
+				orders.add(orderFromResultSet(resultSet));
+			}
+			return orders;
+		} catch (SQLException e) {
+
+		}
+		return new ArrayList<>();
+	}
+
+
+	public Order create(Order order) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("insert into Orders (customer_id, total_cost, number_of_items) values('" + order.getCustomer_Id()
+					+ "','" + order.getTotal_Cost() + "', '" + order.getNumber_Of_Items() + "')");
+
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+
+
+	public Order update(Order order) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("update Orders set customer_id ='" + order.getCustomer_Id() + "', total_cost ='"
+					+ order.getTotal_Cost() + "', number_of_items ='"+ order.getNumber_Of_Items() + "' where id =" + order.getOrder_Id());
+
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * Deletes a customer in the database
+	 *
+	 * @param id - id of the customer
+	 */
+	@Override
+	public void delete(long id) {
+		try (Connection connection = DriverManager.getConnection(jdbcConnectionUrl, username, password);
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("delete from Orders where id = " + id);
+		} catch (Exception e) {
+			LOGGER.debug(e.getStackTrace());
+			LOGGER.error(e.getMessage());
+		}
+	}
+
+
+
+}
